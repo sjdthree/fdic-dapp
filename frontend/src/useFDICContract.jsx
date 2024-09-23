ce4befe43462fa603180b2b608a96b92bb31f6ca
adit313 <app2116@gmail.com>
2024-09-23 09:43:10 -0400

    Resolved merge conflicts and merged add_regulator branch

--- frontend/src/useFDICContract.jsx
+++ frontend/src/useFDICContract.jsx
@@ -1,21 +1,21 @@
 // src/useFDICContract.js
-import { useContext, useEffect, useState, useMemo } from 'react';
-import { BlockchainContext } from './BlockchainProvider';
-import OnChainFDIC from './abis/OnChainFDIC.json';
-import { Contract } from 'ethers';
+import { useContext, useEffect, useState, useMemo } from "react";
+import { BlockchainContext } from "./BlockchainProvider";
+import OnChainFDIC from "./abis/OnChainFDIC.json";
+import { Contract } from "ethers";
 
 export const useFDICContract = () => {
   const { provider, selectedChain } = useContext(BlockchainContext);
   const [fdicContract, setFdicContract] = useState(null);
 
-  const contractAddresses = useMemo (
+  const contractAddresses = useMemo(
     () => ({
-    Ethereum: '0xYourEthereumContractAddress', // Replace with your contract address on Ethereum
-    Polygon: '0xYourPolygonContractAddress',   // Replace with your contract address on Polygon
-    PolygonAmoy: '0xe97c2190996c7661657a07bD114844b36A9882c2',
-    PolygonZKevm_test: '0xe97c2190996c7661657a07bD114844b36A9882c2',
-    Ganache: '0x596a58959872f44d4ad96CAa9443BB7217ba73A1',   // Replace with your Ganache contract address
-    // Add more as needed
+      Ethereum: "0xYourEthereumContractAddress", // Replace with your contract address on Ethereum
+      Polygon: "0xYourPolygonContractAddress", // Replace with your contract address on Polygon
+      PolygonAmoy: "0x43b8f9acd8bf1b9f20d630affe54df019938a4ed",
+      PolygonZKevm_test: "0xe97c2190996c7661657a07bD114844b36A9882c2",
+      Ganache: "0x596a58959872f44d4ad96CAa9443BB7217ba73A1", // Replace with your Ganache contract address
+      // Add more as needed
     }),
     []
   );
@@ -26,18 +26,24 @@ export const useFDICContract = () => {
         try {
           const signer = await provider.getSigner();
           // Set default chain to "PolygonAmoy" if selectedChain is null or undefined
-          const activeChain = selectedChain || 'PolygonAmoy';
+          const activeChain = selectedChain || "PolygonAmoy";
           const contractAddress = contractAddresses[activeChain];
 
           if (!contractAddress) {
             console.error(`Contract address not found for ${activeChain}`);
             return;
           }
-          console.log(`Using contract address: ${contractAddress} for chain: ${activeChain}`);
-          const contract = new Contract(contractAddress, OnChainFDIC.abi, signer);
+          console.log(
+            `Using contract address: ${contractAddress} for chain: ${activeChain}`
+          );
+          const contract = new Contract(
+            contractAddress,
+            OnChainFDIC.abi,
+            signer
+          );
           setFdicContract(contract);
         } catch (error) {
-          console.error('Error initializing contract:', error);
+          console.error("Error initializing contract:", error);
         }
       } else {
         setFdicContract(null);
