668864675152fa5c762f3cfebcc7981475da4b10
sjdthree <steved@sloan.mit.edu>
2024-09-29 21:29:13 -0400

    new Current Banking Market but also may break whitescreen

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-94c373db'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fvghpe2hqt8"
+    "revision": "0.mav0s502l1g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
