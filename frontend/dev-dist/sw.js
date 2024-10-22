998aa7c6d01d38991238c9d661157012b5f6c4ce
sjdthree <steved@sloan.mit.edu>
2024-10-22 16:27:44 -0400

    fix redundant all regulator section

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-94c373db'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.g9bcq4otvo8"
+    "revision": "0.50n3i3u6i6o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
