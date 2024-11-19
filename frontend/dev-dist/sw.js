ec9ec1a1e8d8fb7d540d0dd1873ea2a7113d8d05
sjdthree <steved@sloan.mit.edu>
2024-11-19 08:34:22 -0500

    simple sw.js update

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-94c373db'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4ie7bg2p3ig"
+    "revision": "0.qnos3ac79a"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
